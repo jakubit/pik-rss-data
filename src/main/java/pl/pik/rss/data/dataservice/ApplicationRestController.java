efa96f1b76a355627894ced79e297664ba0a2f92
jakub <pawlak011@gmail.com>
2019-06-02 12:53:12 +0200

    Endpoint for heroku startup

---
+++ src/main/java/pl/pik/rss/data/dataservice/ApplicationRestController.java
@@ -0,0 +1,16 @@
+package pl.pik.rss.data.dataservice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationRestController {
+
+    @GetMapping(name = "/")
+    public ResponseEntity herokuStartupEndpoint() {
+        String message = "[PIK RSS Data Service] This service will be running for 20 minutes from now. After this time, service will not be accessible and another request will be required.";
+        return new ResponseEntity(message, HttpStatus.OK);
+    }
+}
